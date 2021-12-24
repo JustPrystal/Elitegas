b79ba0f050f120d2995dc76d4a246b4d183d2777
Mohsin <mohsin.axecorp@gmail.com>
2021-12-23 18:16:48 -0800

    My acc Responsive

--- theme/woocommerce/myaccount/my-address.php
+++ theme/woocommerce/myaccount/my-address.php
@@ -42,9 +42,6 @@ $oldcol = 1;
 $col    = 1;
 ?>
 
-<p>
-	<?php echo apply_filters( 'woocommerce_my_account_my_address_description', esc_html__( 'The following addresses will be used on the checkout page by default.', 'woocommerce' ) ); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?>
-</p>
 
 <?php if ( ! wc_ship_to_billing_address_only() && wc_shipping_enabled() ) : ?>
 	<div class="u-columns woocommerce-Addresses col2-set addresses">
