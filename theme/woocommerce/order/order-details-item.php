3207b6e13ef1da3aeba555063ac0b8eb8eed43c0
Ali <ali.axecorp@gmail.com>
2022-01-19 09:59:57 -0800

    Adding subtitle to my account orders

--- theme/woocommerce/order/order-details-item.php
+++ theme/woocommerce/order/order-details-item.php
@@ -31,7 +31,9 @@ if ( ! apply_filters( 'woocommerce_order_item_visible', true, $item ) ) {
 		$product_permalink = apply_filters( 'woocommerce_order_item_permalink', $is_visible ? $product->get_permalink( $item ) : '', $item, $order );
 
 		echo wp_kses_post( apply_filters( 'woocommerce_order_item_name', $product_permalink ? sprintf( '<a href="%s">%s</a>', $product_permalink, $item->get_name() ) : $item->get_name(), $item, $is_visible ) );
-
+		if(get_field("subtitle",$item['product_id'])){
+			echo " - ".get_field("subtitle",$item['product_id']);
+		}
 		$qty          = $item->get_quantity();
 		$refunded_qty = $order->get_qty_refunded_for_item( $item_id );
 
